b1d839ba46dbe18966266f9d260559d23994751c
MariahAdams <mariah.adams23@yahoo.com>
2018-08-30 16:14:35 -0700

    games added to home

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUser, getGames } from './reducers';
+import { Link } from 'react-router-dom';
 import { requestGame } from './actions';
 import PropTypes from 'prop-types';
 import styles from './Home.css';
@@ -9,11 +10,12 @@ class Home extends Component {
 
   static propTypes = {
     user: PropTypes.object,
+    games: PropTypes.array.isRequired,
     requestGame: PropTypes.func.isRequired
   };
   
   render() { 
-    const { user, requestGame } = this.props;
+    const { user, games, requestGame } = this.props;
 
     return (
       <div className={styles.home}>
@@ -21,6 +23,13 @@ class Home extends Component {
         {
           user && <section>
             <button onClick={requestGame}>Play Game</button>
+            <ul>
+              {games.map((gameKey, i) => (
+                <li key={gameKey}>
+                  <Link to={`/games/${gameKey}`}>Game {i + 1}</Link>
+                </li>
+              ))}
+            </ul>
           </section>
         }
       </div>
